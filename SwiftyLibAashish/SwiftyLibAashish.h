80580a7ef612f6878f46fec4efc29c7c57599cbe
Ashish Katiyar <ashish.katiyar@paytm.com>
2020-11-14 15:28:24 +0530

    Initial project setup

---
+++ SwiftyLibAashish/SwiftyLibAashish.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLibAashish.h
+//  SwiftyLibAashish
+//
+//  Created by Ashish Katiyar on 14/11/20.
+//  Copyright © 2020 Ashish Katiyar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibAashish.
+FOUNDATION_EXPORT double SwiftyLibAashishVersionNumber;
+
+//! Project version string for SwiftyLibAashish.
+FOUNDATION_EXPORT const unsigned char SwiftyLibAashishVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibAashish/PublicHeader.h>
+
+
