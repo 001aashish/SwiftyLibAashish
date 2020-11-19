9bee8ceb55fb1c2fd93b75cafab5f6a53b096e3c
Ashish Katiyar <ashish.katiyar@paytm.com>
2020-11-19 21:30:22 +0530

    merge-1

---
+++ SwiftyLibAashish/Address.swift
@@ -0,0 +1,19 @@
+//
+//  Address.swift
+//  SwiftyLibAashish
+//
+//  Created by Ashish Katiyar on 19/11/20.
+//  Copyright © 2020 Ashish Katiyar. All rights reserved.
+//
+
+import Foundation
+
+class Address {
+    let houseNo: String
+    let street: String
+    
+    init() {
+        houseNo = ""
+        street = ""
+    }
+}
